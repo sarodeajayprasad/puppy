40d3f1a3836ffe7375c75dccef2f127c07bd8ec2
Ajay Prasad Sarode <sarodeajayprasad@gmail.com>
2023-11-10 19:56:51 +0530

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sarodeajayprasad/puppy
+
+go 1.20
---
+++ puppy.go
@@ -0,0 +1,9 @@
+package puppy
+
+func Bark() string{
+	retrun "Woof!"
+}
+
+func Barks() string {
+	return "Woof! Woof! Woof!"
+}
